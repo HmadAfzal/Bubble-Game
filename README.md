56758edefc5a1700dffa119517a0f1c7d1da4f2e
Hmad Afzal <hmadafzal00@gmail.com>
2024-02-11 23:32:30 +0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # Bubble-Game
+
+## Description
+Bubble Game is a simple browser-based game where players must click on bubbles to increase their score. Each bubble has a number inside, and players must click on the bubble with the same number as the target number displayed. The game ends when the timer runs out. Players aim to achieve the highest score possible within the given time.
+
+## How to Play
+2. **Click on Bubbles**: Click on bubbles with numbers matching the target number displayed at the top. Each correct click increases your score.
+3. **Watch the Timer**: Keep an eye on the timer at the top. The game ends when the timer reaches zero.
+4. **Score**: Your score is displayed on the screen. Try to beat your high score in subsequent rounds!
+
+## Technologies Used
+- HTML
+- CSS
+- JavaScript
+
+## Features
+- Randomly generated bubbles with numbers.
+- Randomly generated target number.
+- Timer countdown.
+- Score tracking.
+- End-of-game message displaying the final score.
+
+## Preview
+![Bubble Game Preview](bubble-game-preview.png)
+
+## How to Run
+1. Clone the repository to your local machine.
+2. Open `index.html` in your web browser.
+3. Click on the "Start" button to begin playing the game.
+
+## Credits
+- Created by [Hmad Afzal]
---
+++ script.js
@@ -0,0 +1,55 @@
+let pbottom=document.querySelector(".pbottom")
+let button=document.querySelector(".start")
+
+let hit=document.querySelector(".hit")
+ var time=60;
+ let score=document.querySelector(".score")
+ score.innerText=0;
+ function increaseScore() {
+    let currentScore = parseInt(score.innerText);
+    score.innerText = currentScore + 10;
+}
+function makeBubble(){
+    for (let index = 0; index <112; index++) {
+         let bubble=document.createElement("div");
+    bubble.classList.add("bubble")
+    bubble.innerText=Math.floor(Math.random()*10)+1;
+    pbottom.appendChild(bubble)
+    }
+   }
+
+
+function randhit() {
+    let hitNo=Math.floor(Math.random()*10)+1;
+    hit.innerText=hitNo
+}
+function timmer() {
+    const timerint = setInterval(() => {
+        if (time >= 0) {
+            let timer = document.querySelector(".time");
+            timer.innerText = time--;
+        } else {
+            clearInterval(timerint);
+            pbottom.innerHTML=`<h2 class="yourscore"><h1>Time's Up<br><span>Score: </span>${score.innerText}</h1></h2>`
+            
+        }
+    }, 1000);
+}
+
+pbottom.addEventListener("click",(param)=>{
+    let clickedNum=param.target.innerText;
+    if (clickedNum==hit.innerText) {
+        randhit();
+        increaseScore();
+    }
+    else{
+       
+    }
+})
+
+
+   makeBubble();
+randhit();
+timmer();
+
+ 
